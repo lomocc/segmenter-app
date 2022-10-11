7cfcab0fffd24043f92c135188d89ace70ba916c
vincent <vincent@rockx.com>
2022-10-11 11:17:21 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-base text-white">
+        <img
+          src={logo}
+          className="h-[40vmin] pointer-events-none motion-safe:animate-[spin_20s_linear_infinite]"
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="text-[#61dafb]"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
